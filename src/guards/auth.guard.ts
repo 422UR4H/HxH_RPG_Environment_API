8784c35efdd9ecdccd50225624e57d4361dce1ce
422UR4H <tiagosindra@id.uff.br>
2023-12-07 21:08:59 -0300

    :sparkles: Feat: health and add guards and decorators

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+// import { Observable } from 'rxjs';
+import { AuthService } from 'src/auth/auth.service';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { authorization } = request.headers;
+
+    try {
+      const data = this.authService.checkToken(
+        authorization.split(' ')[1] ?? '',
+      );
+      // .split(' '[1]);
+      const user = await this.usersService.findOne(data.sub);
+      request.user = user;
+    } catch (err) {
+      throw new UnauthorizedException('Access Denied!');
+    }
+    return true;
+  }
+}
