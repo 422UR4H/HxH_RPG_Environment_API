0b1265cb68ae2165c0a081668d3a1701097d10fe
422UR4H <tiagosindra@id.uff.br>
2023-12-09 19:56:58 -0300

    :bug: Fix: return data

--- src/users/dto/output-user.dto.ts
+++ src/users/dto/output-user.dto.ts
@@ -1,13 +1,19 @@
-import { User } from '@prisma/client';
+import { Role, User } from '@prisma/client';
 
 export class OutputUserDto {
   id: string;
   nick: string;
   email: string;
+  role: Role;
+  created_at: Date;
+  updated_at: Date;
 
   constructor(user: User) {
     this.id = user.id;
     this.nick = user.nick;
     this.email = user.email;
+    this.role = user.role;
+    this.created_at = user.created_at;
+    this.updated_at = user.updated_at;
   }
 }
