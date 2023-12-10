01146a7548f2381710ce27aa9452581fc25a0446
422UR4H <tiagosindra@id.uff.br>
2023-12-10 14:41:49 -0300

    :sparkles: Fix: roll in Dice

--- src/domain/dice/dice.ts
+++ src/domain/dice/dice.ts
@@ -1,9 +1,9 @@
 import { v4 as uuid } from 'uuid';
 
-export class Dice {
+export abstract class Dice {
   // function and parameter name semantics:
   // roll d20 or roll d6 for example
-  roll(d: number): number {
+  static roll(d: number): number {
     return uuid().replace(/\D/g, '') % d;
   }
 }
