8d58879dddc86b2e16f87cffc15e52225956ad5b
422UR4H <tiagosindra@id.uff.br>
2023-12-11 05:10:47 -0300

    :construction_worker: Build: use helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(helmet());
   await app.listen(process.env.PORT || 3000);
   console.log(`server is running on port ${process.env.PORT || 3000}`);
 }
